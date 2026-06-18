568a453657f5551e116da976d10baccc8f0c313e
marcella grau <marcellagrau3@gmail.com>
2026-06-18 14:17:21 -0300

    Add files via upload

---
+++ script-8.js
@@ -0,0 +1,7 @@
+let quantidadeProduto = prompt("Informe que on produto está disponível")
+
+if (quantidadeProduto >= 34) {
+    alert('O produto está disponível')
+} else {
+    alert('O produto está indisponível')
+}
